c37b5539aa556fcc43b6e1189bef3c8d76ed7b02
Kfriske00 <korbyn.friske@outlook.com>
2021-03-10 19:06:57 -0600

    Refactoring for Dependency Inversion.

---
+++ ConsoleClassLibrary/IRegex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleClassLibrary
+{
+    public interface IRegex
+    {
+        string regexPattern { get; set; }
+
+    }
+}
---
+++ ConsoleClassLibrary/IRoster.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleClassLibrary
+{
+    public interface IRoster
+    {
+        List<Student> personRoster { get; set; }
+
+        void AddStudent(Student student);
+        void PrintRoster();
+        string ToString();
+    }
+}
