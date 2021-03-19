3af2239ccaad884dc332da50f3d90c97a7cd963c
Kfriske00 <korbyn.friske@outlook.com>
2021-03-19 00:21:51 -0500

    Added comment to Roster class

--- ConsoleClassLibrary/Rosters/Roster.cs
+++ ConsoleClassLibrary/Rosters/Roster.cs
@@ -10,7 +10,7 @@ namespace ConsoleClassLibrary
     public class Roster : IRoster
     {
         public string rosterName;
-        //To reference the SchoolLogic class and avoid circlular dependencies I would have to create a third project and just have the other 2 projects reference it. Exactly the same thing I am doing with the SchoolLogic class. 
+        //To reference the SchoolLogic class and avoid circlular dependencies I would have to create a third project and just have the other 2 projects reference it. Exactly the same thing I am doing with the SchoolLogic class. (Reminder of why I need to do this: it will crash because I haven't actually created a new list yet. I am trying to use my School Logic to do that)
         public List<IPerson> personRoster { get; set; }
         public Roster(string name)
         {
